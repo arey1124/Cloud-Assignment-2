fda0c6cd7688d0c631d3a607e51d8bc1695590f2
Arihant <arihant0996@gmail.com>
2024-01-20 09:58:03 -0400

    Added code changes for app container 2

--- A1/src/container-app-2/app.js
+++ A1/src/container-app-2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const axios = require('axios');
+const csv = require('csv-parser');
 
 const app = express();
 const port = 6001;
@@ -9,12 +10,41 @@ app.use(express.json());
 
 app.post('/process', async (req, res) => {
   try {
-    
+    const { file, product } = req.body;
+    const csvData = await parseCSV(file);
+
+    var sum = 0;
+    for (const row of csvData) {
+        const prod = row.product;
+        if(prod == product) {
+            const amount = parseInt(row.amount);
+            sum+=amount;
+        }
+    }
+    res.json({
+        file: file,
+        sum: sum,
+    });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+
+function parseCSV(file) {
+    return new Promise((resolve, reject) => {
+      const results = [];
+      const stream = fs.createReadStream(file);
+  
+      stream
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => resolve(results))
+        .on('error', (error) => reject(error));
+    });
+  }
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
