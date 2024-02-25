478ed8f3b05050defc9704c50933771c680de2c7
Arihant <arihant0996@gmail.com>
2024-02-25 01:47:59 -0400

    Added changes for assignment 2

---
+++ A2/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+var mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
+var connection = mysql.createConnection({
+  host     : "database-2.c36ssy0co52j.us-east-2.rds.amazonaws.com",
+  user     : "admin",
+  password : "Cloud123",
+  port     : "3306",
+  database : "Cloud2",
+});
+
+// Route to store products
+app.post('/store-products', (req, res) => {
+  const products = req.body.products;
+
+  if (!Array.isArray(products)) {
+    return res.status(400).send(products);
+  }
+
+  products.forEach(product => {
+    const query = `INSERT INTO products (name, price, availability) VALUES (?, ?, ?)`;
+    connection.query(query, [product.name, product.price, product.availability], (err, result) => {
+      if (err) {
+        console.error('Error inserting product: ', err);
+        return res.status(500).send('Internal Server Error');
+      }
+    });
+  });
+
+  return res.status(200).send({"message" : "Success."});
+});
+
+// Route to list products
+app.get('/list-products', (req, res) => {
+  connection.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      console.error('Error querying products: ', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    return res.status(200).json({"products" :results});
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
